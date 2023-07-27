f9c47d5a7bdcec0be29f339ef9b09170b2b77cc7
seumomo <xo0207@naver.com>
2023-07-27 15:45:52 +0900

    07.storage 학습 완료

--- client/main.js
+++ client/main.js
@@ -1,3 +1,25 @@
-console.log('hello js!');
+import { deleteStorage, getNode, getStorage, setStorage } from './lib/index.js';
 
-const a = 10;
+const textField = getNode('#textField');
+const button = getNode('button');
+
+function handleTextField() {
+  const value = this.value;
+
+  setStorage('text', value);
+}
+
+function init() {
+  getStorage('text').then((res) => {
+    textField.value = res;
+  });
+}
+
+function handleClear() {
+  deleteStorage();
+  textField.value = '';
+}
+
+textField.addEventListener('input', handleTextField);
+window.addEventListener('DOMContentLoaded', init);
+button.addEventListener('click', handleClear);
